4bf233e0e7d4accfb5ea67159b77c924940af5df
arciol <arciool.13@gmail.com>
2025-04-25 14:30:07 +0200

    modify dao

--- app/src/main/java/com/example/qoolquotes/data/Quote.kt
+++ app/src/main/java/com/example/qoolquotes/data/Quote.kt
@@ -1,14 +1,12 @@
 package com.example.qoolquotes.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "quotes")
 data class Quote(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val content: String,
-    val source: String,
-    val author: String,
-    val image: String,
-    val audio: String,
+    @ColumnInfo(name = "text") val text: String,
+    @ColumnInfo(name = "author") val author: String
 )
--- app/src/main/java/com/example/qoolquotes/data/QuoteDao.kt
+++ app/src/main/java/com/example/qoolquotes/data/QuoteDao.kt
@@ -1,11 +1,17 @@
 package com.example.qoolquotes.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface QuoteDao {
-    @Query("SELECT * FROM quotes ORDER BY author ASC")
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertQuote(quote: Quote)
+
+    @Query("SELECT * FROM quotes")
     fun getAllQuotes(): Flow<List<Quote>>
+
 }
