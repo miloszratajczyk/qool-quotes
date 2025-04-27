f0c47f0645820196fc983998addf69c07eca09d0
Milosz Ratajczyk <miloszratajczyk07@gmail.com>
2025-04-27 22:29:39 +0200

    Extend `QuoteDao` functionality

--- app/src/main/java/com/example/qoolquotes/data/QuoteDao.kt
+++ app/src/main/java/com/example/qoolquotes/data/QuoteDao.kt
@@ -1,9 +1,11 @@
 package com.example.qoolquotes.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -11,7 +13,41 @@ interface QuoteDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertQuote(quote: Quote)
 
+    @Delete
+    suspend fun delete(quote: Quote)
+
+    @Update
+    suspend fun updateQuote(quote: Quote)
+
+
     @Query("SELECT * FROM quotes")
-    fun getAllQuotes(): Flow<List<Quote>>
+    suspend fun getAllQuotes(): Flow<List<Quote>>
+
+    @Query("SELECT COUNT(*) FROM quotes")
+    suspend fun getAllQuoteCount(): Int
+
+
+    @Query("SELECT * FROM quotes WHERE photo IS NOT NULL AND photo != ''")
+    suspend fun getQuotesWithImages(): Flow<List<Quote>>
+
+    @Query("SELECT COUNT(*) FROM quotes WHERE photo IS NOT NULL AND photo != ''")
+    suspend fun getQuotesWithImagesCount(): Int
+
+
+    @Query("SELECT * FROM quotes WHERE audio IS NOT NULL AND audio != ''")
+    suspend fun getQuotesWithAudio(): Flow<List<Quote>>
+
+    @Query("SELECT COUNT(*) FROM quotes WHERE photo IS NOT NULL AND photo != ''")
+    suspend fun getQuotesWithAudioCount(): Int
+
+    @Query("""
+        SELECT * FROM quotes 
+        WHERE text LIKE '%' || :query || '%' 
+        OR author LIKE '%' || :query || '%' 
+        OR source LIKE '%' || :query || '%' 
+        OR photo IS NOT NULL AND photo != '' 
+        OR audio IS NOT NULL AND audio != ''
+    """)
+    suspend fun searchQuotes(query: String): Flow<List<Quote>>
 
 }
